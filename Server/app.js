35d2c6094e94facd5a6288319252e145063e5550
mfy <Faisaly1234@outlook.com>
2021-03-09 16:34:43 +0000

    added endpoints

--- Server/app.js
+++ Server/app.js
@@ -2,3 +2,28 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 
+const webpages = require("./data")
+
+app.use(cors());
+
+app.get('/', (req, res) => res.send('Hello World!!!!'))
+
+app.get('/searchresults', (req, res) => res.send("test object") )
+
+app.get('/randomresult', (req, res) => {
+    console.log(webpages)
+    try{
+    res.redirect(getRandomPage())
+    }catch(e) {
+        console.log(e.message)
+    }
+})
+
+function getRandomPage () {
+    let len = webpages.length
+    let index =  Math.floor(Math.random() * len)
+    return webpages[index].url
+}
+ 
+module.exports = app;
+
---
+++ Server/data.js
@@ -0,0 +1,10 @@
+
+websites = [{url:"https://www.google.com", title: "Google", 
+description: "Search the world's information, including webpages, images, videos and more. Google has many special features to help you find exactly what you're looking" },
+{url:"https://www.youtube.com", title: "Youtube", 
+description: "Enjoy the videos and music you love, upload original content, and share it all with friends, family, and the world on YouTube." },
+{url:"https://outlook.live.com/", title: "Outlook -free personal email and calendar from Microsoft", 
+description: "Sign in to access your Outlook, Hotmail or Live email account." }
+]
+
+module.exports =websites;
