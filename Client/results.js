2def240fdbebace53d6cde7250a88b5754c6baa4
sem-the-dev <semhar.tesfu@outlook.com>
2021-03-10 12:20:58 +0000

    add js to results file

---
+++ Client/results.js
@@ -0,0 +1,27 @@
+function searchResults(){
+    console.log()
+    fetch('http://localhost:3000/searchresults')
+    .then(res => res.json())
+    .then(responseData => {
+        responseData.forEach(website => renderData(website))
+    })
+    .catch(err => console.log(err.message))
+}
+
+function renderData({url, title, description}) {
+    let results = document.querySelector('section')
+    let website = document.querySelector('#website')
+    let newWebsite = document.createElement('p')
+    let urlLink = document.querySelector('#url-result')
+    let newUrlLink = document.createElement('p')
+    let urlTitle = document.querySelector('#page-name')
+    let newUrlTitle = document.createElement('p')
+    let descrip = document.querySelector('#page-description')
+    let newDescrip = document.createElement('p')
+    newUrlLink.textContent= url
+    newUrlTitle.innerHTML =   `<a href = "https://en.wikipedia.org/" a> ${title} </a>`
+    newDescrip.textContent = description
+    website.append(newUrlLink, newUrlTitle, newDescrip)
+    results.append(newWebsite)
+}
+searchResults()
